9aa37e22455d55f1978673f8db3d282e66d4bd9b
Joy Wangari <warugujoy@gmail.com>
2016-04-26 16:53:10 +0300

    New module added

---
+++ day_2/super_sum.py
@@ -0,0 +1,25 @@
+
+
+def super_sum(A):
+	"""
+	Takes a list A, and:
+		-Halves every even number
+		-Doubles every odd number
+	And returns sum of all
+	"""
+	total = 0
+	for item in A:
+		if item%2 == 0:
+			item=item/2
+			total += item
+			
+		else:
+			item=item*2
+			total += item 
+			
+	return total
+
+print super_sum([1,2,3,4,5])
+print super_sum([2,3])
+
+	#pass
