e610620b536a4d92df2f5af64d8dbbbcd3e4398b
Joy Wangari <warugujoy@gmail.com>
2016-04-27 14:51:56 +0300

    Add args_kwargs functions

--- day_3/args_kwargs.py
+++ day_3/args_kwargs.py
@@ -17,3 +17,68 @@ for item in people:
 for name,age in people:
 	print hello(name,age)
 '''
+
+def super_sum(*args):
+	'''
+	Takes in variable number of items,
+	and returns the sum.
+	e.g. super_sum(10, 20) =30
+		super_sum(10,20,40) =70
+		super_sum(1,4,5,6,7) =23
+	'''
+
+	total = 0
+	for i in args:
+		total += i
+	return total
+
+print super_sum(10, 20)
+print super_sum(1, 4, 5, 6, 7)
+a = [10, 40, 60]
+print super_sum(*a)
+
+print 
+print "_______________________"
+print "At least two parameters"
+print "________________________"
+
+def super_sum(a,b,*args):
+	'''
+	Takes in variable number of items,
+	and returns the sum.
+	e.g. super_sum(10, 20) =30
+		super_sum(10,20,40) =70
+		super_sum(1,4,5,6,7) =23
+	'''
+
+	total = 0
+	for i in args:
+		total += i
+	return total
+
+print super_sum(10, 20)
+print super_sum(1, 4, 5, 6, 7)
+a = [10, 40, 60]
+print super_sum(*a)
+
+print 
+print "_______"
+print "Kwargs "
+print "_______"
+
+print "Kwargs in a dictionary entry"
+print
+
+def hello_again(**kwargs):
+	return "I am {}, and I'm {}".format(kwargs['name'], kwargs['age'])
+
+print hello_again(name='Joe', age=20)
+print hello_again(age=20, name='Joe')
+
+print
+print "Kwargs in a dictionary"
+print
+
+joe = {'name': 'Joe', 'age':98}
+print hello_again(**joe)
+print hello_again(name='Joe', age=98)
