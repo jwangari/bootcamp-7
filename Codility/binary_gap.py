b204a8d7ad1014623f84a6b70d509dd4137cb842
Joy Wangari <warugujoy@gmail.com>
2016-04-29 15:53:08 +0300

    Add binary

--- Codility/binary_gap.py
+++ Codility/binary_gap.py
@@ -1,5 +1,7 @@
-
 def solution(N):
+	"""
+	Returns the number passed into the argument as a binary list
+	"""
 	b = []
 	new_list = []
 	
@@ -11,12 +13,31 @@ def solution(N):
 	new_list = b [::-1]
 	count = 0
 
-	for item in new_list:
-		if item == 1:
-			
+	return new_list
 
+def solution1(N):
+	"""
+	Returns the binary gap
+	"""
+	current_gap = 0
+	max_gap = 0
 
+	if N > 0 and N % 2 == 0:
+		N //= 2
+
+	while N > 0:
+		reminder = N % 2
+		if reminder == 0:
+			current_gap += 1
+		else:
+			if current_gap != 0:
+				max_gap = max(current_gap, max_gap)
+				current_gap = 0
+		N //= 2
+	return max_gap
 
 print solution(9)
 print solution(529)
+print solution1(9)
+print solution1(529)
 
